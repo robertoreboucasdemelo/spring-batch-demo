0fbc2192be1a202d2dbc2cc2c0d464131f468c7b
Roberto Reboucas de Melo <roberto.reboucas@ibm.com>
2022-12-06 13:30:17 -0300

    Add Incrementer and Catch Externar Job Parameters

--- spring-batch-demo/src/main/java/com/moduretick/springbatchdemo/BatchConfig.java
+++ spring-batch-demo/src/main/java/com/moduretick/springbatchdemo/BatchConfig.java
@@ -6,10 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,22 +31,29 @@ public class BatchConfig {
 		return jobBuilderFactory
 				.get("printHelloJob")
 				.start(printHelloStep())
+				.incrementer(new RunIdIncrementer())
 				.build();
 	}
 	
 	public Step printHelloStep() {
 		return stepBuilderFactory
 				.get("printHelloStep")
-				.tasklet(new Tasklet() {
-
-					@Override
-					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
-							throws Exception {
-						System.out.println("Hello Batch");
-						return RepeatStatus.FINISHED;
-					}	
-				})
+				.tasklet(printHelloTasklet(null))
 				.build();
+	}
+
+	@Bean
+	@StepScope
+	public Tasklet printHelloTasklet(@Value("#{jobParameters['name']}")String name) {
+		return new Tasklet() {
+
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+					throws Exception {
+				System.out.println(String.format("Hello Batch, %s!", name));
+				return RepeatStatus.FINISHED;
+			}	
+		};
 	}
 
 }
