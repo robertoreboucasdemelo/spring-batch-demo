9512225a6f6e86a90892c86dcf843ca550c98022
Roberto Reboucas de Melo <roberto.reboucas@ibm.com>
2022-12-08 23:48:27 -0300

    Externalize application.properties File

---
+++ src/main/java/com/moduretick/springbatchdemo/config/PropsConfig.java
@@ -0,0 +1,18 @@
+package com.moduretick.springbatchdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class PropsConfig {
+	
+	@Bean
+	public PropertySourcesPlaceholderConfigurer config() {
+		PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+		configurer.setLocation(new FileSystemResource("C:\\Users\\015099631\\Documents\\SpringBatch\\config\\application.properties"));
+		return configurer;
+	}
+
+}
